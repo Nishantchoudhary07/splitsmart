2bff186f45be51671922ca540ee230a9638653ed
Nishant Choudhary <nishantsahu046@gmail.com>
2021-04-24 13:26:00 +0530

    commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# print("Welcome To The Split Smart Calculator")
+# total_bill = int(input("What is the total bill?: "))
+# number_of_people = int(input("How many people are splitting the bill?: "))
+# print("\n")
+# payment_per_person = int(total_bill / number_of_people)
+# total_bill_float = int(total_bill)
+# tip_and_total = total_bill_float
+# print(f"Total bill : tip_and_total")
+# print(f"Each person owes: {payment_per_person}")
+
+# not complete yet.
