97c10ba7cb72ddec090e55bbb317288a83674321
Nishant Choudhary <nishantsahu046@gmail.com>
2021-04-29 16:48:32 +0530

    added the add expense function completely

--- main.py
+++ main.py
@@ -9,14 +9,25 @@ while True:
         break
     elif rn == 2:
         print("You have chosen 2 'Add Expenses'. ")
-        print('enter the comma separated names of the members.')
-        aa = input()
-        print('Total amount paid.')
-        bb = int(input())
-        d = aa,bb
+        print('Enter the space separated names of the members:')
+        members_names = (input())
+        print('Total amount paid:')
+        amount_paid = int(input())
+        z = members_names.split()
+        d = z ,amount_paid
+        emp_list = []
+
+        num_of_members = len(z)
+        p = amount_paid/(num_of_members+1)
+        for i in range(len(z)):
+            y=(emp_list.append(z[i]))
+            for i in range(1):
+                j = (emp_list.append(p))
+        print(emp_list)
         with open(person, 'a') as filecope:
-            for leadset in d:
+            for leadset in emp_list:
                 filecope.write('%s\n' % leadset)
+
 
         g = open(person, 'r')
         cc = (g.readlines())
@@ -29,6 +40,5 @@ while True:
 file = open(person, 'a')
 # file.write('I Paid: rs.'+str(exp)+'\n')
 file.close()
-
 
 
