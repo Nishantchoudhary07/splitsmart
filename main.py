9d897f22740b06e1025adb82aa77822a3dab2ec9
Nishant Choudhary <nishantsahu046@gmail.com>
2021-09-10 13:25:23 +0530

    completer splitsmart

--- main.py
+++ main.py
@@ -6,6 +6,22 @@ while True:
     rn = int(input('Enter 1 or 2: '))
     if rn == 1:
         print("You have chosen 'View Balance'.")
+        with open(person, 'r') as t:
+            d = t.readlines()
+            i = 0
+            no_user = len(d) / 2
+
+            print(no_user)
+            all_user = []
+            for user in range(int(no_user)):
+                u = []
+                u.sort()
+                for x in range(2):
+                    u.append(d[i + x])
+                i = +2
+
+                all_user.append(u)
+            print(all_user)
         break
     elif rn == 2:
         print("You have chosen 2 'Add Expenses'. ")
@@ -14,7 +30,7 @@ while True:
         print('Total amount paid:')
         amount_paid = int(input())
         z = members_names.split()
-        d = z ,amount_paid
+        d = z, amount_paid
         emp_list = []
 
         num_of_members = len(z)
@@ -31,14 +47,5 @@ while True:
 
         g = open(person, 'r')
         cc = (g.readlines())
-        # g.close()
+        g.close()
         break
-    else:
-        print('Please enter no. 1 or 2')
-
-
-file = open(person, 'a')
-# file.write('I Paid: rs.'+str(exp)+'\n')
-file.close()
-
-
